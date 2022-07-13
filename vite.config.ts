688fd84b2c5f3a74aa50bc5f88690a73889db843
liwenkang <changzhiliwenkang@outlook.com>
2022-07-13 15:14:41 +0800

    feat: finish new article

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import eslint from 'vite-plugin-eslint';
 export default defineConfig({
   plugins: [react(), eslint()],
   server: {
+    host: '0.0.0.0',
     port: 4000, // 你需要定义的端口号
     open: true, // open支持boolean/string类型，为true时打开默认浏览器，为string类型时，打开指定浏览器，具体查看官网即可
     proxy: {
